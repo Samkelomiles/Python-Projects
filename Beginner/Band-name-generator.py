03902a3685e30ac7fe893126db21364a46393dfd
Samkelo Mvelase <samkelo10@gmail.com>
2022-07-09 21:01:46 +0200

    code update

--- Beginner/Band-name-generator.py
+++ Beginner/Band-name-generator.py
@@ -1,2 +1,4 @@
-print("Github-test")
-print("remote-test")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is your pets name?\n")
+
+print("Your band name could be "+ " " + city_name+ " " + pet_name) 
