f7aa9680047176f70ec2f58bd798901a186761d2
Samkelo Mvelase <samkelo10@gmail.com>
2022-07-10 17:20:39 +0200

    type converted

--- Beginner/Tip-calculator.py
+++ Beginner/Tip-calculator.py
@@ -1,12 +1,12 @@
-# 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
 
-####################################
-#Write your code below this line 👇
-first_num = int(two_digit_number[0])
-second_num = int(two_digit_number[1])
-print(first_num + second_num)
+#=================== TIP CALCULATOR SECTION =======================
+print("Welcome to the tip calculator.\n")
+bill = float(input("What was the total bill: R"))
+tip  = int(input("What percentage would you like to give (10,12 or 15) - "))
+num_of_people = int(input("How many people to split the bill: "))
 
+bill += bill*(tip/100)
+split = round((bill/num_of_people),2)
 
-#Mathematical operators
+print(f"Each person should pay: R{split}")
+
